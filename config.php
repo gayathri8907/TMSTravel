52fd53d61e1f7a6700df0a96b56c56d7fb3706d5
gayathri8907 <67545965+gayathri8907@users.noreply.github.com>
2020-06-29 02:11:23 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,13 +3,13 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','root');
+define('DB_PASS','');
 define('DB_NAME','tms');
 $dsn = "mysql:host=localhost;port=3306;charset=utf8;dbname=tms";
 // Establish database connection.
 try
 {
-$dbh = new PDO($dsn,"root","root");
+$dbh = new PDO($dsn,"root","");
 }
 catch (PDOException $e)
 {
