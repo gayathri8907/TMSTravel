61da02ce53bb24d56c773ad7d58728cb7fc32368
gayathri8907 <67545965+gayathri8907@users.noreply.github.com>
2020-06-29 01:53:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
+define('DB_HOST','172.30.121.123');
 define('DB_USER','root');
 define('DB_PASS','root');
 define('DB_NAME','tms');
-$dsn = "mysql:host=localhost;port=3306;charset=utf8;dbname=tms";
+$dsn = "mysql:host=172.30.121.123;port=3306;charset=utf8;dbname=tms";
 // Establish database connection.
 try
 {
