243aff3eaf15b107d63528fc05d60674535c5489
gayathri8907 <67545965+gayathri8907@users.noreply.github.com>
2020-07-01 01:40:21 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv(MYSQL_SERVICE_PORT);
+$dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv(databasename);
 try{
-$connection = new mysqli($dbhost, $dbport, $dbuser, $dbpwd, $dbname);
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connection failed: %s\n", $mysqli->connect_error);
     exit();
